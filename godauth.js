a7606d29eaf45d3ab05cf56b25dee48086b84ac0
Zoltan TOTH <zoltan.toth@prezi.com>
2013-10-10 14:24:02 +0200

    works

--- godauth.js
+++ godauth.js
@@ -1,91 +1,94 @@
 "use strict";
 var util = require("util"),
-    crypto = require('crypto'),
-    shasum = crypto.createHash('sha1');
+    crypto = require('crypto')
 
+function GodAuth(cookie_secret){
 
-if (typeof process.env.GODAUTH_COOKIE_SECRET === "undefined") {
-    console.error("Can't find environmental variable GODAUTH_COOKIE_SECRET");
-    process.exit(101);
+	this._sha1sum = function(str) {
+		var shasum = crypto.createHash('sha1');
+		shasum.update(str);
+		return shasum.digest('hex');
+	}
+
+	this._cleanUserAgent = function(agentStr) {
+		if (/[\w\W]*AppleWebKit[\w\W]*/.test(agentStr)) {
+			return "StupidAppleWebkitHacksGRRR";
+		}
+		return agentStr;
+	}
+
+	this.authenticate = function(email, roles, timestamp, signature, userAgent) {
+		var rawCookie = util.format("%s-%s-%s-%s", email, roles, timestamp, this._cleanUserAgent(userAgent)),
+		    signatureBase = this._secret + rawCookie,
+		    computedSignature = this._sha1sum(signatureBase),
+		    currentTime = new Date().getTime() / 1000;
+
+		timestamp = parseInt(timestamp, 10);
+		
+		if (computedSignature !== signature) {
+			return null;
+		}
+
+		
+		if (timestamp < currentTime - 8 * 60 * 60) {
+			return null;
+		}
+
+		if (timestamp > currentTime + 5 * 60) {
+			return null;
+		}
+
+		return {
+			'email': email,
+			'roles': roles,
+			'timestamp': timestamp,
+			'signature': signature
+		};
+	}
+
+	this.authenticateCookie = function(cookieValue, userAgent) {
+		var match = /^([\w\W]+)\-([a-zA-Z0-9]+)\-([0-9]+)\-([a-z0-9]+)$/.exec(cookieValue);
+		
+		if (!match || match.length < 5) {
+			return null;
+		}
+		return this.authenticate(match[1], match[2], match[3], match[4], userAgent);
+	}
+
+	this.authenticateRequest = function(request, response) {
+		var cookies = {},
+		    auth,
+		    authToken;
+
+		if (request.headers.cookie) {
+			request.headers.cookie.split(';').forEach(
+					function (cookie) {
+					var parts = cookie.split('=');
+					cookies[parts[0].trim()] = (parts[1] || '').trim();
+					}
+					);
+		}
+
+		if (!cookies.authtoken) {
+			auth = null;
+		} else {
+			authToken = cookies.authtoken.replace(/^"+|"+$/g, '');
+			auth = this.authenticateCookie(authToken, request.headers["user-agent"]);
+		}
+
+		if (auth === null) {
+			response.writeHead(301,
+					{ Location: 'https://prezi.com/api/v2/auth/godauth/?ref=' + encodeURIComponent(request.url) }
+					);
+			response.end();
+		}
+		
+		return auth;
+	}
+
+	this._secret = cookie_secret;
 }
-var godauthCookieSecret = process.env.GODAUTH_COOKIE_SECRET;
 
-function sha1sum(str) {
-	shasum.update(str);
-	return shasum.digest('hex');
-}
-
-function cleanUserAgent(agentStr) {
-	if (/[\w\W]*AppleWebKit[\w\W]*/.test(agentStr)) {
-		return "StupidAppleWebkitHacksGRRR";
-	}
-	return agentStr;
-}
-
-function authenticate(email, roles, timestamp, signature, userAgent) {
-	var rawCookie = util.format("%s-%s-%s-%s", email, roles, timestamp, cleanUserAgent(userAgent)),
-	    signatureBase = godauthCookieSecret + rawCookie,
-	    computedSignature = sha1sum(signatureBase),
-        currentTime = new Date().getTime() / 1000;
-
-	timestamp = parseInt(timestamp, 10);
-
-	if (computedSignature !== signature) {
-		return null;
-	}
-
-	if (timestamp < currentTime - 8 * 60 * 60) {
-		return null;
-	}
-
-	if (timestamp > currentTime + 5 * 60) {
-		return null;
-	}
-
-	return {
-		'email': email,
-		'roles': roles,
-		'timestamp': timestamp,
-		'signature': signature
-	};
-}
-
-function authenticateCookie(cookieValue, userAgent) {
-	var match = /^([\w\W]+)\-([a-zA-Z0-9]+)\-([0-9]+)\-([a-z0-9]+)$/.exec(cookieValue);
-	if (match.length < 5) {
-		return null;
-	}
-	return authenticate(match[1], match[2], match[3], match[4], userAgent);
-}
-
-function authenticateRequest(request, response) {
-    var cookies = {},
-        auth;
-
-	if (request.headers.cookie) {
-		request.headers.cookie.split(';').forEach(
-			function (cookie) {
-	            var parts = cookie.split('=');
-	            cookies[parts[0].trim()] = (parts[1] || '').trim();
-			}
-		);
-	}
-
-	if (!cookies.authtoken) {
-		auth = null;
-	} else {
-		auth = authenticateCookie(cookies.authtoken, request.headers["user-agent"]);
-	}
-
-	if (auth === null) {
-		response.writeHead(301,
-			{ Location: 'https://prezi.com/api/v2/auth/godauth/?ref=' + encodeURIComponent(request.url) }
-			);
-		response.end();
-		return null;
-	}
-}
-
-exports.authenticate = authenticate;
-exports.authenticateCookie = authenticateCookie;
-exports.authenticateRequest = authenticateRequest;
+module.exports.create = function(cookie_secret) {
+	return new GodAuth(cookie_secret);
+};
